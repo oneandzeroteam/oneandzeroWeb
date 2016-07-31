b9ec1d48d497d8159fe675587685f1cb924cd042
Nicholas Kim <nicholaskim94@gmail.com>
2016-07-31 17:40:27 +0900

    Fix bug of sign_ups not accepting parameters

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: "registrations"}
 
   root "pages#show", page: "home"
   get "/pages/:page" => "pages#show"
