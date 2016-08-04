064d0c1fd5d7431ba6ef251f064043508f422677
pjo901018 <pjo901018@korea.ac.kr>
2016-08-04 18:34:53 +0900

    1.Post Model 추가 / 2.Comment Model 추가

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+  
   devise_for :users, controllers: {registrations: "registrations"}
 
   root "pages#show", page: "home"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803053955) do
+ActiveRecord::Schema.define(version: 20160804084538) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user"
+    t.string   "references"
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "members", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +33,15 @@ ActiveRecord::Schema.define(version: 20160803053955) do
     t.boolean  "is_alumni",    default: false
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
