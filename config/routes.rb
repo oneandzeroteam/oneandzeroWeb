7068cff390a681edb663ffadd0efb90967a55728
서가람 <skekrkfka@gmail.com>
2016-08-09 07:17:54 +0000

    change 'pagename' of root -> indeDx

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: {registrations: "registrations"}
 
-  root "pages#show", pagename: "home"
+  root "pages#show", pagename: "index"
   get "/pages/:pagename" => "pages#show"
 
   get "/board/:boardname" => "posts#index"
