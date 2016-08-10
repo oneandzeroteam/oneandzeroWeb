fd71cacf50b6a00679613797fbeec12bcb94fc55
Nicholas Kim <nicholaskim94@gmail.com>
2016-08-10 23:30:50 +0900

    Fix broken link for create_member

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
   post "/board/:boardname/posts/" => "posts#create"
   get "/board/:boardname/posts/:post_id" => "posts#show"
 
-  #post "/members/new" => "members#new"
   get "/members/" => "members#index"
   get "/alumni" => "members#alumni"
   get "/professor" => "members#professor"
@@ -26,6 +25,7 @@ Rails.application.routes.draw do
   get "/admin/users" => "admin#index_users"
   get "/admin/members/" => "admin#index_members"
   get "/admin/members/new" => "admin#new_member"
+  post "/admin/members" => "admin#create_member"
   delete "/admin/users/:userid" => "admin#destroy_user"
   delete "/admin/members/:memberid" => "admin#destroy_member"
 
