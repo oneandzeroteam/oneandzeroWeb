ceb7939b315bd0a508b6d8e1f6d07a257250beeb
root <root@oaz.korea.ac.kr>
2016-08-13 00:28:38 +0900

    Recruit 수정

--- public/javascripts/recruit.js
+++ public/javascripts/recruit.js
@@ -1,3 +1,8 @@
 $(document).ready(function () {
-	console.log("hihihihi");
+	$("#recruitform").on("ajax:success", function (e, data, status, xhr) {
+		alert("Success!");
+	});
+	$("#recruitform").on("ajax:error", function (e, xhr, status, error) {
+		alert("Fail...");
+	});
 });
