4fa971b94a962743a9961a91d7509e12a1afe3fe
root <root@oaz.korea.ac.kr>
2016-08-16 14:22:51 +0900

    로그인시 이전 페이지로 이동

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,19 @@ class ApplicationController < ActionController::Base
     @current_projects = Project.where(is_ongoing: true).limit(6)
     @past_projects = Project.where(is_ongoing: false).limit(10)
   end
+
+  after_filter :store_action
+  def store_action
+	  return unless request.get?
+	  if (request.path != "/users/sign_in" &&
+		  request.path != "/users/sign_up" &&
+		  request.path != "/users/sign_out" &&
+		  request.path != "/users/password/new" &&
+		  request.path != "/users/password/edit" &&
+		  request.path != "/users/confirmation" &&
+		  !request.xhr?) # don't store ajax calls
+
+		  store_location_for(:user, request.fullpath)
+	  end
+  end
 end
